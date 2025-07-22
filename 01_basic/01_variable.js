6933d845224599cc5fe0752a7148b49e71505b8c
MirzaFaisal14581 <mirzafaisal799126@gmail.com>
2025-07-22 06:17:04 +0000

    01_variable in node js

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=12546
+let accountEmail="mirza@gmail.com"
+var accountpassword="799126"
+accountcity="chennai"
+
+
+//accountId=2151//not allowed
+accountEmail="h@h@gmail.com"
+accountpassword="15462"
+console.log("accountId");
+let accountState;
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
