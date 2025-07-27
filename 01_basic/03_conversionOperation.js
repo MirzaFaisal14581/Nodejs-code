6364c542e607df031f6fc7744df7e6ea52da7f41
MirzaFaisal14581 <mirzafaisal799126@gmail.com>
2025-07-27 06:37:45 +0000

    03_conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let value= 3
+let negValue=++value
+//console.log(negValue)
+
+
+let str1=2;
+let str2=3;
+let str3=str1-str2;
+//console.log(str3);
+
+//console.log("2"+3);
+//console.log(2+2+"3");
+//console.log(2+"2"+5);
+//console.log("2"+"5");
+
+//console.log(+true);
+//console.log(true);
+//console.log(+"")
+
+let gameCounter=150
+--gameCounter;
+console.log(gameCounter);
+
+
+
+
